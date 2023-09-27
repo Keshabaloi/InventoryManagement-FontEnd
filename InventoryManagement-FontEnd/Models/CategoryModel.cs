b683c8aea589d9313ebbd71553253d92dc033284
Aloi Team <aloi@techkraftinc.com>
2023-09-27 22:29:24 +0545

    Add project files.

---
+++ InventoryManagement-FontEnd/Models/CategoryModel.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagement_FontEnd.Models
+{
+    public class CategoryModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public bool DeleteFlag { get; set; }
+    }
+}
